9312385487e1d13b7cd19419c6c798caa2ddb30d
Artem <artem.smirnov2005@mail.ru>
2023-12-28 10:32:28 +0300

    First commit!

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-# telegram_sender_code
-### mostly useless tool bot funny
-
-Installing library:
-
-`python3 -m pip install --upgrade telethon`
-> the time library was installed on python by default 
-
-### Code:
-```python
 from telethon.sync import TelegramClient
 import time
 
-user_id = '' # your telegram id
+user_id_2 = '' # your telegram id
 api_id = 0  # your telegram api id
 api_hash = "" # your telegram api hash (you can get it on https://my.telegram.org)
 send_id = '' # the person who you send
@@ -19,7 +9,7 @@ message = ''
 repeat = 1 # how many times you want send message
 shedule = 0 # if repeat > 1 with Time you can send message every given seconds
 
-sender = TelegramClient(user_id, api_id, api_hash)
+sender = TelegramClient(user_id_2, api_id, api_hash)
 
 async def main():
     for i in range(repeat):
@@ -28,7 +18,3 @@ async def main():
 
 with sender:
     sender.loop.run_until_complete(main())
-```
-
-### difficulty with my.telegram.org:
-visit this [website](https://lavhost.su/telegram-api)
